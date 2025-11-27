6fe2d87d7f893f8f185a26131749848ba4fe4035
Pritish Tomar <pritishtomar2004@gmail.com>
2025-11-27 12:50:43 +0530

    update: constants for storage and messages

---
+++ engine/src/main/java/in/brewquery_engine/constants/MessageConstants.java
@@ -0,0 +1,9 @@
+package in.brewquery_engine.constants;
+
+public class MessageConstants {
+    public static final String NO_DATASET_FOUND = "NO_DATASET_FOUND";
+    public static final String ERROR_CREATING_CONNECTION = "ERROR_CREATING_CONNECTION";
+    public static final String INTERNAL_SERVER_ERROR = "INERNAL_SERVER_ERROR";
+    public static final String SESSION_CREATED_SUCCESSFULLY = "SESSION_CREATED_SUCCESSFULLY";
+    public static final String SESSION_REFRESHED ="SESSION_REFRESHED"; 
+}
--- engine/src/main/java/in/brewquery_engine/constants/StorageConstants.java
+++ engine/src/main/java/in/brewquery_engine/constants/StorageConstants.java
@@ -1,5 +1,7 @@
 package in.brewquery_engine.constants;
 
 public class StorageConstants {
-    public static final String BasePath = "/mnt/brewquery/datasets"; 
+    public static String BASE_DIR_PATH = "/mnt/brewquery/datasets/"; 
+    public static final Integer  REDIS_TTL_SESSION = 20; // 20 minutes
+     
 }
