43e6fa912a789e3fe7804f45ea60af2260351b42
Pritish Tomar <pritishtomar2004@gmail.com>
2025-11-27 13:04:24 +0530

    add-query-request-dto-for-sql-execution

---
+++ engine/src/main/java/in/brewquery_engine/entities/query/SQLQueryRequestDTO.java
@@ -0,0 +1,10 @@
+package in.brewquery_engine.entities.query;
+
+import lombok.Data;
+
+@Data
+public class SQLQueryRequestDTO {
+    private String sessionId;
+    private String query;
+    private String expectedQuery;
+}
