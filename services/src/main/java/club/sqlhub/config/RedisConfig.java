2bbde2df5186d12b6f370eb0cfeb34d72d26d77d
Pritish Tomar <pritish.tomar.dev@gmail.com>
2026-09-19 23:45:28 +0530

    fix: connect to Upstash Redis via full rediss:// URL with SSL

--- services/src/main/java/club/sqlhub/config/RedisConfig.java
+++ services/src/main/java/club/sqlhub/config/RedisConfig.java
@@ -1,8 +1,13 @@
 package club.sqlhub.config;
 
+import io.lettuce.core.RedisURI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -11,19 +16,38 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.url}")
+    private String redisUrl;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisURI uri = RedisURI.create(redisUrl);
+
+        RedisStandaloneConfiguration standaloneConfig = new RedisStandaloneConfiguration();
+        standaloneConfig.setHostName(uri.getHost());
+        standaloneConfig.setPort(uri.getPort());
+        if (uri.getUsername() != null) {
+            standaloneConfig.setUsername(uri.getUsername());
+        }
+        if (uri.getPassword() != null && uri.getPassword().length > 0) {
+            standaloneConfig.setPassword(RedisPassword.of(new String(uri.getPassword())));
+        }
+
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder builder =
+                LettuceClientConfiguration.builder();
+        if (redisUrl.startsWith("rediss://")) {
+            builder.useSsl();
+        }
+
+        return new LettuceConnectionFactory(standaloneConfig, builder.build());
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
-
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-
         return template;
     }
 
